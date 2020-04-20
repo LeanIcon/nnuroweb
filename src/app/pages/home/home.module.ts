0513bd1c21abe000876d10b139d3db4362dfb3b6
gaby96 <gabrieladdo64@gmail.com>
2020-04-20 22:23:33 +0000

    wholesalerpage

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -19,6 +19,8 @@ import { ContactComponent } from '../contact/contact.component';
 import { TestiComponent } from '../testi/testi.component';
 import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
 import { ParticlesModule } from 'angular-particle';
+import { FormpageComponent } from '../formpage/formpage.component';
+
 
 import { Index1Component } from './index1/index1.component';
 import { HomeAboutComponent } from '../home-about/home-about.component';
@@ -46,7 +48,8 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
     CovidComponent,
     PrivacyPolicyComponent,
     HomeAboutComponent,
-    ShopComponent
+    ShopComponent,
+    FormpageComponent,
     // ComponentToLoad,
   ],
   imports: [
